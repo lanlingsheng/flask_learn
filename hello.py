aec81edf8c575c0675f8f46a76185881aa1a457d
lanlingsheng <gaoyan987@gmail.com>
2017-06-10 15:31:45 +0800

    添加jinja2 渲染模板

--- hello.py
+++ hello.py
@@ -2,17 +2,20 @@
 __author__ = 'lenovo'
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Your Brower is %s<h1>' %user_agent
+    #user_agent = request.headers.get('User-Agent')
+    #return '<h1>Your Brower is %s<h1>' %user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s<h1>' % name
+    #return '<h1>Hello, %s<h1>' % name
+    return render_template('user.html', name = name)
 
 if __name__ =='__main__':
     app.run(debug=True)
