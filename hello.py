69c293961c47440b52254be6e5c76c96e021b658
lanlingsheng <gaoyan987@gmail.com>
2017-06-10 21:45:18 +0800

    增加静态文件和 url_for()

--- hello.py
+++ hello.py
@@ -9,27 +9,31 @@ from flask_script import Manager
 app = Flask(__name__)
 
 manager = Manager(app)
-boostrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
-    #user_agent = request.headers.get('User-Agent')
-    #return '<h1>Your Brower is %s<h1>' %user_agent
+    # user_agent = request.headers.get('User-Agent')
+    # return '<h1>Your Brower is %s<h1>' %user_agent
     return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-    #return '<h1>Hello, %s<h1>' % name
-    return render_template('user.html', name = name)
+    # return '<h1>Hello, %s<h1>' % name
+    return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
