4c8931b7892cfe61e236c6013bbb9e18b36b53a2
lanlingsheng <gaoyan987@gmail.com>
2017-06-11 14:51:56 +0800

    第4章 web表单。 添加 Flask-WTF

--- hello.py
+++ hello.py
@@ -7,19 +7,33 @@ from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gy18977949946//'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # user_agent = request.headers.get('User-Agent')
     # return '<h1>Your Brower is %s<h1>' %user_agent
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
